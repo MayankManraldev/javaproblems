ed33f00c060330862ae71b35a1cf89e670fd74f2
Mayank Manral <mayankmanral29@gmail.com>
2024-06-21 13:43:34 +0530

    Added closest sum program

--- src/program/corejava/ArraysProgram.java
+++ src/program/corejava/ArraysProgram.java
@@ -21,4 +21,19 @@ public class ArraysProgram {
         }
         return null;
     }
+
+    private static int[] closestSum(int[] arr) {
+        Arrays.sort(arr);
+        int l=0,r= arr.length-1;
+        while(l<r){
+            int sum=arr[l]+arr[r];
+            if(sum<0){
+                l++;
+            } else if (sum>0) {
+                r--;
+            }else
+                return new int[] {arr[l],arr[r]};
+        }
+        return null;
+    }
 }
