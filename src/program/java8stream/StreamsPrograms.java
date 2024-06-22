98a99064f55f2792adbe6389c42d043104d8871a
Mayank Manral <mayankmanral29@gmail.com>
2024-06-22 17:55:27 +0530

    Added Stream Programs

--- src/program/java8stream/StreamsPrograms.java
+++ src/program/java8stream/StreamsPrograms.java
@@ -1,14 +1,29 @@
 package program.java8stream;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamsPrograms {
     public static void main(String[] args) {
-        int []arr={1,2,3,3,4,5,6,6,7,7,9,8};
+        int[] arr = {1, 2, 3, 3, 4, 5, 6, 6, 7, 7, 9, 8};
+        List<String> collected = Arrays.stream(arr).mapToObj(n -> n + "").filter(a -> a.startsWith("1")).collect(Collectors.toList());
+        System.out.println("get List of number start with A" + collected);
+
+        List<String> lst = Arrays.asList("Mayank", "Garisha", "jsd");
+        String s = lst.stream().max(Comparator.comparing(o -> o.length()))
+                .get();
+
+        System.out.println("Highest Length String is " + s);
+
+        String str = "mayankmanral";
+        String s1 = Arrays.stream(str.toLowerCase().split("")).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == 1L)
+                .map(entry -> entry.getKey())
+                .findFirst().get();
+        System.out.println("fisrt nonrepeating character is " + s1);
+
         List collect = Arrays.stream(arr).boxed().filter(value -> value % 2 == 0)
                 .peek(System.out::println).collect(Collectors.toList());
         System.out.println(collect);
@@ -23,8 +38,7 @@ public class StreamsPrograms {
                                 Function.identity(), Collectors.counting()
                         )
                 );
-        System.out.println("Count occurence of each word" + result);
-
+        System.out.println("Count occurrence of each word" + result);
 
 
     }
